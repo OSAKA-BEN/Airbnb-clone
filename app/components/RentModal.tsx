df6f87d99c8115e864deac584ac7f3eaddac7058
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2024-11-14 12:47:02 +0900

    feat: :sparkles: Select will all countries & Visualize location on a map with leaflet (step 2 rent modal)

--- app/components/RentModal.tsx
+++ app/components/RentModal.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useMemo, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import useRentModal from "../hooks/useRentModal";
 import { categories } from "./Categories";
 import CategoryInput from "./CategoryInput";
+import CountrySelect from "./CountrySelect";
 import Heading from "./Heading";
 import Modal from "./Modal";
 
@@ -44,7 +46,14 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
 
+  const Map = useMemo(
+    () => dynamic(() => import("./Map"), { ssr: false }),
+    [location]
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
@@ -77,7 +86,7 @@ const RentModal = () => {
     return "Back";
   }, [step]);
 
-  const bodyContent = (
+  let bodyContent = (
     <div className="flex flex-col gap-8">
       <Heading
         title="Which of these best describes your place?"
@@ -98,12 +107,28 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
   return (
     <Modal
       title="Airbnb your home!"
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
